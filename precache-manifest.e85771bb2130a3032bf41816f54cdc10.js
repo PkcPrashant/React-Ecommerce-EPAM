70827d08c93eee7f4a7ee445204516092d2c6dfe
Prashant Choudhary <Prashant_Choudhary@epam.com>
2020-06-07 15:52:26 +0530

    Updates

---
+++ precache-manifest.e85771bb2130a3032bf41816f54cdc10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f23b6afd7e20c295c5cf13464cd362",
+    "url": "/React-Ecommerce-EPAM/index.html"
+  },
+  {
+    "revision": "7c562943ee8d866dbeb1",
+    "url": "/React-Ecommerce-EPAM/static/css/3.30c2d978.chunk.css"
+  },
+  {
+    "revision": "f9f094679b2dbd18d322",
+    "url": "/React-Ecommerce-EPAM/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9e1976fe43be6e26ebca",
+    "url": "/React-Ecommerce-EPAM/static/js/0.6e8807dd.chunk.js"
+  },
+  {
+    "revision": "7c562943ee8d866dbeb1",
+    "url": "/React-Ecommerce-EPAM/static/js/3.68ad2d67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Ecommerce-EPAM/static/js/3.68ad2d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f094679b2dbd18d322",
+    "url": "/React-Ecommerce-EPAM/static/js/main.345ebc4a.chunk.js"
+  },
+  {
+    "revision": "abf57dd656542be23f67",
+    "url": "/React-Ecommerce-EPAM/static/js/runtime-main.138e8d6e.js"
+  }
+]);
